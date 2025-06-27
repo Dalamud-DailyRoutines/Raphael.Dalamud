f20f5e3304a8b25f4a026554f7266f4ba6f1ae5c
sbago <50283553+sbago@users.noreply.github.com>
2025-06-28 00:16:37 +0800

    Update RaphaelBinding.cs

--- Raphael.Dalamud/RaphaelBinding.cs
+++ Raphael.Dalamud/RaphaelBinding.cs
@@ -235,7 +235,7 @@ namespace Raphael.Dalamud
         {
             var lvl = recipe.RecipeLevelTable.Value;
             var baseProgress = (craftsmanship * 10f / lvl.ProgressDivider) + 2.0f;
-            if (jobLvl < lvl.ClassJobLevel)
+            if (jobLvl <= lvl.ClassJobLevel)
                 baseProgress *= lvl.ProgressModifier / 100f;
             return baseProgress;
         }
@@ -243,7 +243,7 @@ namespace Raphael.Dalamud
         {
             var lvl = recipe.RecipeLevelTable.Value;
             var baseProgress = (control * 10f / lvl.QualityDivider) + 35.0f;
-            if (jobLvl < lvl.ClassJobLevel)
+            if (jobLvl <= lvl.ClassJobLevel)
                 baseProgress *= lvl.QualityModifier / 100f;
             return baseProgress;
         }
